6b16b2a2dc7c015606930f10f326e007d64b5946
Juan Romo <juanromo.dev@gmail.com>
2018-08-08 22:14:01 -0500

    Add overview icon

--- app/src/main/java/com/juanromodev/javatest/ui/home/HomeActivity.java
+++ app/src/main/java/com/juanromodev/javatest/ui/home/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.juanromodev.javatest.ui.home;
 
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +23,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            TypedValue typedValue = new TypedValue();
+            Resources.Theme theme = getTheme();
+            theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+            int color = typedValue.data;
+
+            Bitmap overviewIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_overview);
+            ActivityManager.TaskDescription td = new ActivityManager.TaskDescription(null, overviewIcon, color);
+
+            setTaskDescription(td);
+        }
 
         startTestBtn = findViewById(R.id.start_test_btn);
         startTestBtn.setOnClickListener(new View.OnClickListener() {
